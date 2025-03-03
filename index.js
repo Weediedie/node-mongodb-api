adaa1f701138217d618fc7e8bf5a6d8a9b3889be
Sine Nale <sinenale13@gmail.com>
2025-03-03 11:33:53 +0800

    fix for march launch

--- index.js
+++ index.js
@@ -31,11 +31,6 @@ const userSchema = new mongoose.Schema({
   firstname: { type: String, required: true },
   station1: { type: StationSchema, default: {} },
   station2: { type: StationSchema, default: {} },
-  station3: { type: StationSchema, default: {} },
-  station4: { type: StationSchema, default: {} },
-  station5: { type: StationSchema, default: {} },
-  station6: { type: StationSchema, default: {} },
-  station7: { type: StationSchema, default: {} },
   dateCreated: { type: Date, default: Date.now }
 });
 
@@ -114,7 +109,7 @@ app.get('/api/fetch/user', async (req, res) => {
 });
 
 app.post('/api/insert/user', async (req, res) => {
-  const { firstname, lastname, station1, station2, station3, station4, station5, station6, station7 } = req.body;
+  const { firstname, lastname, station1, station2 } = req.body;
 
   if (!firstname || !lastname) {
     return res.status(400).json({ message: 'No user name provided' });
@@ -134,11 +129,6 @@ app.post('/api/insert/user', async (req, res) => {
       lastname,
       station1: station1 || { status: 'inactive', dateTimeModified: null },
       station2: station2 || { status: 'inactive', dateTimeModified: null },
-      station3: station3 || { status: 'inactive', dateTimeModified: null },
-      station4: station4 || { status: 'inactive', dateTimeModified: null },
-      station5: station5 || { status: 'inactive', dateTimeModified: null },
-      station6: station6 || { status: 'inactive', dateTimeModified: null },
-      station7: station7 || { status: 'inactive', dateTimeModified: null },
     });
 
     await user.save();
